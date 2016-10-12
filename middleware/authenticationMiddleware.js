7fa83016399c8e20d19bfb815b4b9692d934c6cf
Joseph <jrichardson@jmstudios.net>
2016-10-12 09:23:16 -0700

    creating users working need to test properly though

--- middleware/authenticationMiddleware.js
+++ middleware/authenticationMiddleware.js
@@ -4,25 +4,51 @@ var router = express.Router();
 
 var User = require('../models/userModel.js');
 
+var userModel = new User();
 var Cloudant = require("cloudant");
 
 var VCAP = require('../BluemixServices/StudyBuddy_VCAP_Services.json');
 var username = VCAP["cloudantNoSQLDB"][0]["credentials"]["username"]; 
 var password = VCAP["cloudantNoSQLDB"][0]["credentials"]["password"];
 function cloudantMiddleware(req, res, next) {
-    var userModel = new User();
     
     var latestUser = req.locals.newUser;
     userModel.findByPhoneNumber( latestUser.data.phoneNumber ).then(
     function successCallback( searchUser ) {
         console.log("FOUND THE USER!!!!!!!!!!!!!!!!!!");
-        console.log( searchUser );
-        next();
+        console.log(req.locals.newUser.userQuestion);
+        if ( req.locals.userQuestion.toLowerCase()  == "yes" ) {
+            console.log( "time to register new USER!!!!!" );
+            userModel.create( latestUser ).then( 
+            function successCallback( result ) {
+                console.log( "promises are gr8t" );
+                next();        
+            },
+            function failedCallback( error ) {
+                console.log( error );
+            });
+        } 
+        
     },
     function failedCallback( error ) {
-        console.log( "THERE WAAS AN ERROR FINDING THE USER" );
-        var message = 'Welcome to StudyBuddy please response with  yes   to register';
-        res.send('<Response><Message>'+message+'</Message></Response>');
+        
+		if ( req.locals.userQuestion.toLowerCase()  == "yes" ) {
+            
+			userModel.create( latestUser ).then( 
+            function successCallback( result ) {
+                console.log( "promises are gr8t" );
+				res.send('<Response><Message>Welcome to StudyBuddy, ask me a computer science conceptual question./</Message></Response>');
+                next();        
+            },
+            function failedCallback( error ) {
+                console.log( error );
+				res.send('<Response><Message>I am sorry, there was an error, please try again./</Message></Response>');
+            });
+        } 
+        else { 
+        	var message = 'Welcome to StudyBuddy please response with  yes   to register';
+        	res.send('<Response><Message>'+message+'</Message></Response>');
+		}
     });
 };
 
@@ -33,6 +59,14 @@ router.get('/', cloudantMiddleware, function(req, res, next) {
 router.post('/sendMessageBodyToWatson', cloudantMiddleware, function(req, res, next) {
     next();
 });
+
+function createNewUser( userData ) {
+    return new Promise( function ( resolve, reject ) {
+        
+    });
+
+};
+
 module.exports = {
     cloudantHandler: router
 }
--- models/userModel.js
+++ models/userModel.js
@@ -40,19 +40,38 @@ User.prototype.findByPhoneNumber = function( phoneNumber ) {
 		phoneNumber: phoneNumber
 	}
     };
-	var findUserPromise = new Promise( function ( resolve, reject ) {
-    	usersdb.find(phoneNumberSelectorForSearch, function(er, result) {
-			if(result.docs.length == 0 || typeof result == 'undefined' || er) {
-					console.log(er);
-					reject(er);
-		// this goes in the promise then() not here	  		res.send('<Response><Message>Welcome to StudyBuddy, send yes to register FOR FREE. /</Message></Response>');
-			}
-			else {
-				resolve(result);
-			}	  	
-		});
+    var findUserPromise = new Promise( function ( resolve, reject ) {
+    usersdb.find(phoneNumberSelectorForSearch, function(er, result) {
+		    if(result.docs.length == 0 || typeof result == 'undefined' || er) {
+				    console.log(er);
+				    reject(er);
+	    // this goes in the promise then() not here	  		res.send('<Response><Message>Welcome to StudyBuddy, send yes to register FOR FREE. /</Message></Response>');
+		    }
+		    else {
+			    resolve(result);
+		    }	  	
+	    });
+    });
+    return findUserPromise;
+};
+
+User.prototype.create = function ( newUser ) {
+    console.log( "creating a new user" );
+    var createUserPromise = new Promise ( function ( resolve, reject ) {
+	var usersdb = db.use( 'users' );
+	usersdb.insert( newUser, function ( er, result ) {
+	    if ( er  ) {
+	        reject( er );
+	    }
+	    else {
+		console.log( "user created successfully : " );
+		console.log( result );
+		resolve( result );
+	    }
 	});
-	return findUserPromise;
+    });
+
+    return createUserPromise;
 };
 
 
